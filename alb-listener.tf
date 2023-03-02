ece3fff98d3c579c45528bfcae10e513cf379a54
Harshitha <harshithadarshan95@gmail.com>
2023-03-02 10:52:21 +0530

    Autopush

---
+++ alb-listener.tf
@@ -0,0 +1,20 @@
+# Creates listener needed to ALB ; And for this listener, we have to created the rules ;
+# Listener rules should come up from the appropriate component
+
+resource "aws_lb_listener" "private" {
+  count             = var.INTERNAL ? 1 : 0
+
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+        type = "fixed-response"
+
+        fixed_response {
+        content_type = "text/plain"
+        message_body = "I am operational"
+        status_code  = "200"
+        }
+   }
+}
